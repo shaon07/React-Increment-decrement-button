5e6bda57fe18c89b1015233d678fe28517910825
shaon07 <alishaon77@gmail.com>
2021-07-29 11:19:11 +0600

    1 file modified

--- src/Todo.js
+++ src/Todo.js
@@ -9,7 +9,7 @@ function Todo() {
     setName(e.target.value);
   }
   function final() {
-    finalName.unshift(name);
+    finalName.push(name);
     setName('');
     console.log(finalName);
   }
