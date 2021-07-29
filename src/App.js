9df692b562e4beecacf4255ce1ba8d840d3b34a9
shaon07 <alishaon77@gmail.com>
2021-07-29 10:06:48 +0600

    1 file modified

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import './style.css';
 import Test from './test';
 import Shaon from './Shaon';
-// import shaonTest from './useEffect';
 import ShaonTest from './shaonTest';
 import Li from './Li';
 import Todo from './Todo';
